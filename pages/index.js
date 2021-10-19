e270a41ccd4b8dd4e0703756be725d53394abaab
Muskaan <muskaan19010@gmail.com>
2021-10-19 20:12:33 +0530

    deleted api folder from 'pages' directory

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,20 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>IP Tracker</title>
-        <meta name="description" content="IP address/Domain tracker created by Nextjs" />
+        <meta
+          name="description"
+          content="IP address/Domain tracker created by Nextjs"
+        />
         <link rel="icon" href="/favicon.png" sizes="16x16" type="image/png" />
       </Head>
 
-
-<main>
-
-
-
-  hii
-</main>
-      
-     
+      <main>hii</main>
     </div>
-  )
+  );
 }
