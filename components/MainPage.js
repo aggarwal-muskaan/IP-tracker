428fa0acde8d323d68a03d29d67a9017d4c62db4
Muskaan <muskaan19010@gmail.com>
2021-11-17 13:56:24 +0530

    integrate API to get location from ip addresses

--- components/MainPage.js
+++ components/MainPage.js
@@ -1,27 +1,51 @@
 import Image from "next/image";
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import leftIcon from "../assets/icon-arrow.svg";
+import leftIcon from "../public/assets/icon-arrow.svg";
 
 function MainPage({ userIp }) {
-  const findCountryDetails = async () => {
-    const ipAdd = `ipAdress=${userIp}`;
+  const [input, setInput] = useState("");
+
+  const findCountryDetails = async (searchIp = userIp) => {
+    const ipAdd = `ipAddress=${searchIp}`;
+    // const ipAdd = `domain=vercel.org`;
+
     const response = await axios(
       `${process.env.NEXT_PUBLIC_BASE_URL}&${ipAdd}`
     );
+    console.log(searchIp, response);
   };
 
   useEffect(() => {
     findCountryDetails();
   }, []);
 
+  const handleInputChange = (event) => {
+    const { value } = event.target;
+    setInput(value);
+  };
+
+  const handleEnterOnly = (event) => {
+    if (event.key === "Enter") findCountryDetails(event.target.value);
+  };
+
   return (
     <>
       <div>
         <h2>IP Address Tracker</h2>
         <div style={{ backgroundColor: "black" }}>
-          <input type="text" />
-          <Image src={leftIcon} alt="search-icon" />
+          <input
+            type="text"
+            placeholder="Search for any IP address or domain"
+            value={input}
+            onChange={(event) => handleInputChange(event)}
+            onKeyDown={(event) => handleEnterOnly(event)}
+          />
+          <Image
+            src={leftIcon}
+            alt="search-icon"
+            onClick={findCountryDetails}
+          />
         </div>
       </div>
     </>
