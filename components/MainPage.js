0e2c40af53baee80077b48b1939947648ea94d44
Muskaan <muskaan19010@gmail.com>
2021-11-17 17:27:06 +0530

    integrate APIs to get location from domain name & ip address

--- components/MainPage.js
+++ components/MainPage.js
@@ -1,28 +1,37 @@
 import Image from "next/image";
-import React, { useEffect, createRef } from "react";
 import axios from "axios";
+import React, { useEffect, createRef } from "react";
+
+import { findByDomain, findByIp } from "../services/findLocation";
 import leftIcon from "../public/assets/icon-arrow.svg";
 
 function MainPage({ userIp }) {
   const textInput = createRef();
 
-  const findCountryDetails = async (searchIp = userIp) => {
-    const ipAdd = `ipAddress=${searchIp}`;
-    // const ipAdd = `domain=vercel.org`;
-
-    const response = await axios(
-      `${process.env.NEXT_PUBLIC_BASE_URL}&${ipAdd}`
-    );
-    console.log(searchIp, response);
-  };
-
   useEffect(() => {
     findCountryDetails();
   }, []);
 
-  const handleInputChange = (event) => {
-    const { value } = event.target;
-    setInput(value);
+  const findCountryDetails = async (rawUserInput = userIp) => {
+    const userInput = rawUserInput.trim();
+    if (userInput !== userIp) {
+      let response;
+      if (isDomain(userInput)) {
+        // searched input is a Domain name
+        response = await findByDomain(userInput);
+      }
+      // searched input is a IP address
+      else response = await findByIp(userInput);
+      console.log(response);
+    } else {
+      // user's IP
+      const response = await findByIp(userInput);
+    }
+  };
+
+  const isDomain = (userInput) => {
+    const newInput = userInput.replace(/\./g, "");
+    return isNaN(newInput);
   };
 
   const handleEnterOnly = (event) => {
--- services/findLocation.js
+++ services/findLocation.js
@@ -1,1 +1,21 @@
 import axios from "axios";
+
+export const findByIp = async (searchIp) => {
+  const ipAdd = `ipAddress=${searchIp}`;
+  const url = `${process.env.NEXT_PUBLIC_BASE_URL}&${ipAdd}`;
+
+  return axios
+    .get(url)
+    .then((res) => res)
+    .catch((err) => err);
+};
+
+export const findByDomain = async (searchDomain) => {
+  const myDomain = `domain=${searchDomain}`;
+  const url = `${process.env.NEXT_PUBLIC_BASE_URL}&${myDomain}`;
+
+  return axios
+    .get(url)
+    .then((res) => res)
+    .catch((err) => err);
+};
